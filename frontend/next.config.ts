defaee30d4bc0fb054c23b007af3498041705d52
3mg <140186933+sertr4linee@users.noreply.github.com>
2026-04-05 02:16:17 +0200

    feat: update Next.js configuration to include turbopack and external packages

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  bundler: "webpack",
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      crypto: false,
+    };
+    config.externals.push("pino-pretty", "encoding");
+    return config;
+  },
+  serverExternalPackages: [
+    "@abstract-foundation/agw-react",
+    "@abstract-foundation/agw-client",
+  ],
 };
 
 export default nextConfig;
